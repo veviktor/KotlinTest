0df42b3a71d39dce7c0ca206fd4470006323141f
Verebelyi Viktor <viktor@different.hu>
2016-04-07 23:25:08 +0200

    Kotlin extension

---
+++ app/src/main/java/veviktor/hu/kotlintest/MainActivity.kt
@@ -0,0 +1,24 @@
+package veviktor.hu.kotlintest
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+
+        fab.setVisibility(View.GONE);
+        welcomeText.setText(resources.getString(R.string.welcome));
+
+//        val fab = findViewById(R.id.fab) as FloatingActionButton
+//        fab.setOnClickListener { view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show() }
+    }
+}
